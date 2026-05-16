c3e988064cc2e01c0e3108d004030f34b3e0f0bd
Melih <melih@watchtogether.app>
2026-05-17 01:25:14 +0300

    connect production backend

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://watch-together-app-ofq0.onrender.com", {
   transports: ["websocket"],
 });
